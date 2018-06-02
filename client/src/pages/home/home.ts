f13fbf61599214047b4bdb684b09496ffbb995dc
Bharat Saraswat <bsaraswat45@gmail.com>
2018-06-02 23:24:52 +0530

    added firebase db

--- client/src/pages/home/home.ts
+++ client/src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+
 declare let firebase:any;
 
 @Component({
@@ -15,6 +16,27 @@ export class HomePage {
   constructor(public navCtrl: NavController) {
 
   }
+
+
+	setData(){
+		// Initialize Cloud Firestore through Firebase
+		var db = firebase.firestore();
+	  	
+  	db.collection("ANGELHACK-USER-Details").add({
+		    fullname: "Bharat Saraswat",
+		    aadharid: 900080007000,
+		    coordinates: [23, 67]
+		})
+		.then(function(docRef) {
+		    console.log("Document written with ID: ", docRef.id);
+		})
+		.catch(function(error) {
+		    console.error("Error adding document: ", error);
+		});
+	}
+
+
+
   authLogin(){
   	firebase.auth().signInWithEmailAndPassword(this.email, this.password)
   	.then(function(){
@@ -27,4 +49,5 @@ export class HomePage {
 	  alert(errorCode);
 	});
   }
+
 }
